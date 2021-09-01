a53a648c37728ce2daa70347bb6946a1c056cb43
ManniG24 <egatica51@gmail.com>
2021-09-01 16:23:14 -0600

    finsished the articles

--- components/Article.js
+++ components/Article.js
@@ -86,6 +86,27 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'Lambda School Students: "We\'re the best!"',
+    date: 'Nov 5th, 2018',
+    firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
+        moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
+        watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
+        Hutt calamari darth jabba.Darth dooku amidala organa moff.Boba darth binks solo hutt skywalker dantooine skywalker.Qui - gonn
+        jar twi'lek jinn leia jango skywalker mon.`,
+
+    secondParagraph: `Grievous fett calamari anakin skywalker hutt.Alderaan darth kenobi darth r2- d2
+        windu mothma.Sidious darth calamari moff.Wampa mothma sith wedge solo mara.Darth gonk maul sith moff chewbacca palpatine
+        mace amidala.C - 3po solo skywalker anakin yoda leia.Maul wampa bespin watto jade ewok darth jabba.Lando dantooine moff
+        k - 3po dantooine luke.Fisto mandalore darth wedge c - 3p0 ahsoka.Secura moff palpatine fett.Anakin sith darth darth.Moff
+        solo leia ben ponda jade.Binks jango aayla skywalker skywalker cade.Mustafar darth ventress anakin watto.Yavin jawa sebulba
+        owen jinn tatooine sith organa.`,
+
+    thirdParagraph: `Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
+        naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff baba wicket
+        han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé wookiee. Leia
+        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   }
 ];
 
@@ -102,7 +123,9 @@ const data = [
 
     <span class="expandButton">+</span>
   </div>
+*/
 
+/*
   Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
   This listener should toggle the class 'article-open' on div.article.
 
@@ -114,3 +137,64 @@ const data = [
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
+
+// {
+//   title,
+//   date,
+//   firstParagraph,
+//   secondParagraph,
+//   thirdParagraph,
+// }
+
+function makeArticle(title, date, one, two, three){
+
+  //creating elements
+  const articleDiv = document.createElement('div');
+  const articleHeader = document.createElement('h2');
+  const articleDate = document.createElement('p');
+  const articleContentOne = document.createElement('p');
+  const articleContentTwo = document.createElement('p');
+  const articleContentThree = document.createElement('p');
+  const articleButton = document.createElement('span');
+
+
+  //appending elements to the article Div
+  articleDiv.appendChild(articleHeader);
+  articleDiv.appendChild(articleDate);
+  articleDiv.appendChild(articleContentOne);
+  articleDiv.appendChild(articleContentTwo);
+  articleDiv.appendChild(articleContentThree);
+  articleDiv.appendChild(articleButton);
+
+  //adding classNames to elements
+  articleDiv.classList.add('article');
+  articleDate.classList.add('date');
+  articleButton.classList.add('expandButton');
+
+  //adding content
+  articleHeader.textContent = title;
+  articleDate.textContent = date;
+  articleContentOne.textContent = one;
+  articleContentTwo.textContent = two;
+  articleContentThree.textContent = three;
+  articleButton.textContent = '+';
+
+  //adding event listener to button
+  articleButton.addEventListener('click', () => {
+    articleDiv.classList.toggle('article-open');
+  })
+  return articleDiv;
+
+}
+
+
+
+const article = document.querySelector('.articles');
+
+data.forEach(objectData => {
+  const { title, date, firstParagraph, secondParagraph, thirdParagraph } = objectData;
+
+  const articles = makeArticle(title, date, firstParagraph, secondParagraph, thirdParagraph);
+
+  article.appendChild(articles);
+})
